8a10c73ffdaa561cc2ea39fd7e57cebacaf998d4
maximaarminger <maxima.arminger@htl-salzburg.ac.at>
2020-01-29 08:45:13 +0100

    return value

---
+++ Assets/MyGame/Scripts/ExerciseReturnValue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExerciseReturnValue : MonoBehaviour
+{
+    public float randomMin;
+    public float randomMax;
+    void Start()
+    {
+        var IntReturnVariable = IntReturn();
+        var FloatReturnVariable = FloatReturn();
+        var StringReturnVariable = StringReturn();
+        Debug.Log(IntReturnVariable);
+        Debug.Log(FloatReturnVariable);
+        Debug.Log(StringReturnVariable);
+    }
+
+    private void Update()
+    {
+        Debug.Log(Random.Range(randomMin, randomMax));
+    }
+    public void NoReturn()
+    {
+    }
+    public int IntReturn()
+    {
+        int intR2 = 14;
+        return intR2;
+    }
+    public float FloatReturn()
+    {
+        float floatR = 12;
+        return floatR;
+    }
+    public string StringReturn()
+    {
+        string stringR = "Hello";
+        return stringR;
+    }
+}
